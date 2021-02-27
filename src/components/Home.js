849b20a41f14a1e14ead0a9b6db56fe44f8b6dd6
faiza203 <fiazfaiza981@gmail.com>
2021-02-27 10:25:41 -0500

    Add Services Li

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Services from "./Services";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Services />
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Services = () => {
-  <div>Services</div>;
+  return (
+    <div>
+      <ul>
+        <li>Business Strategy</li>
+        <li>Research</li>
+        <li>Data Analysis</li>
+        <li>UI Design</li>
+        <li>UX Design</li>
+        <li>Technology</li>
+        <li>Creative</li>
+      </ul>
+      <div></div>
+    </div>
+  );
 };
 
 export default Services;

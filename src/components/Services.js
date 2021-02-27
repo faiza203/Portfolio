60259b95800429bde412a09245b32bfafb0e2f4c
faiza203 <fiazfaiza981@gmail.com>
2021-02-27 10:32:12 -0500

    Add Learn More Detail

--- src/components/Services.js
+++ src/components/Services.js
@@ -12,7 +12,18 @@ const Services = () => {
         <li>Technology</li>
         <li>Creative</li>
       </ul>
-      <div></div>
+      <div>
+        <h1>Business Strategy</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt
+          voluptate, quibusdam sunt iste dolores consequatur Inventore fugit
+          error iure nisi reiciendis fugiat illo pariatur quam sequi quod iusto
+          facilis officiis nobis sit quis molestias asperiores rem, blanditiis!
+          Commodi exercitationem vitae deserunt qui nihil ea, tempore et quam
+          natus quaerat doloremque.
+        </p>
+        <button>Learn More</button>
+      </div>
     </div>
   );
 };

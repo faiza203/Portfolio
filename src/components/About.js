6782fbf6962f0a47848e908551e77ffdd9908912
faiza203 <fiazfaiza981@gmail.com>
2021-02-27 14:32:18 -0500

    Add Staff for images

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,13 @@ import Staff from "./Staff";
 const About = () => {
   return (
     <div>
+      <div>
+        <h3>About Me</h3>
+        <h1>HI! My name is Faiza Fiaz</h1>
+        <p>
+          I am 12<sup>th</sup> years old Pakistani Developer
+        </p>
+      </div>
       <Staff />
     </div>
   );
--- src/components/Staff.js
+++ src/components/Staff.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Staff = () => {
-  return <div>Staff</div>;
+  return (
+    <div>
+      <h1>Our Staff</h1>
+      <p>
+        Far far away, behind the word mountains, far from the country Pakistan
+      </p>
+      <div>
+        <div>
+          <div className="amirImage backgroundImgDiv"></div>
+          <p>Amir Sadiq Ali</p>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Staff;

3f51d39579b9d2a78e8718c93a2089d15a365090
faiza203 <fiazfaiza981@gmail.com>
2021-02-28 14:37:41 -0500

    Delete setupTests.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Projects = () => {
-  <div>Projects</div>;
+  return (
+    <div>
+      <h1>Our Projects</h1>
+      <div></div>
+      <div></div>
+      <div></div>
+      <h1>......</h1>
+    </div>
+  );
 };
 
 export default Projects;

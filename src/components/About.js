cfa940a83ec478296ce013c15f55912800066631
faiza203 <fiazfaiza981@gmail.com>
2021-02-28 11:39:05 -0500

    Style about Me

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,20 @@
 import React from "react";
 import Staff from "./Staff";
-
+import "./css/about.css";
 const About = () => {
   return (
-    <div>
+    <div className="aboutPage">
       <div className="aboutMe">
         <div className="developerImage"></div>
-        <div>
-          <h3>About Me</h3>
-          <h1>HI! My name is Faiza Fiaz</h1>
-          <p>
-            I am 12<sup>th</sup> years old Pakistani Developer
+        <div className="text-muted mt-3 mb-3">
+          <h1 className="h1 mt-5">
+            HI! My name is
+            <span className="parrot"> Faiza Fiaz</span>
+          </h1>
+          <p className="h3 mt-3">
+            I am 12<sup className="parrot">th</sup> years old Developer from{" "}
+            <br />
+            <span className="parrot">Pakistan </span>
           </p>
         </div>
       </div>

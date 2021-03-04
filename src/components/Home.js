bd38c130430029604dee1f73072daffc501ddd86
faiza203 <fiazfaiza981@gmail.com>
2021-03-04 18:24:55 -0500

    Rotate Paragraph

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React  from "react";
+import './css/home.css'
 
 const Home = () => {
   return (
     <div id="Home" className="d-inline-flex">
       <div className="d-inline-flex">
-          <p>Welcome</p>
+          <p className="rotateP">Welcome</p>
         <div>
           <p>Welcome</p>
           <p>Small Details</p>

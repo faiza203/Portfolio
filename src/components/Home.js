0bdbe870f9753e1a18a7b856941631bd28bb20bf
faiza203 <fiazfaiza981@gmail.com>
2021-03-04 18:22:49 -0500

    Write Structure

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React  from "react";
 
 const Home = () => {
   return (
-    <div id="Home">
-      Home
+    <div id="Home" className="d-inline-flex">
+      <div className="d-inline-flex">
+          <p>Welcome</p>
+        <div>
+          <p>Welcome</p>
+          <p>Small Details</p>
+          <button className="btn">Learn More</button>
+        </div>
       </div>
+   <img src="https://i.insider.com/5638c96cbd86ef16008c6c2f?width=1100&format=jpeg&auto=webp" alt="img"  />
+</div>
   );
 };
 

19382a06ac06de1fc779d85ff970448da0d51e7e
faiza203 <60883181+faiza203@users.noreply.github.com>
2021-03-06 11:52:55 -0800

    Improve Styles

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,34 +3,82 @@ import { FaBeer } from "react-icons/fa";
 import "./css/services.css";
 
 const Services = () => {
+  const titleIDS = [
+    "Business Strategy",
+    "Research",
+    "Data Analysis",
+    "UI Design",
+    "UX Design",
+    "Technology",
+    "Creative",
+  ];
+  const addTitle = function (title) {
+    document.getElementById(title).classList.add("visible-after");
+    titleIDS.forEach(function (titleID) {
+      if (titleID !== title) {
+        document.getElementById(titleID).classList.remove("visible-after");
+      } else {
+        document.getElementById(title).classList.add("visible-after");
+      }
+    });
+  };
+
   return (
     <div className="services" id="Services">
       <ul className="titles">
-        <li className="title">
+        <li
+          className="title visible-after"
+          id="Business Strategy"
+          onClick={() => addTitle("Business Strategy")}
+        >
           <FaBeer className="titleIcon" />
           <p>Business Strategy</p>
         </li>
-        <li className="title">
+        <li
+          className="title"
+          id="Research"
+          onClick={() => addTitle("Research")}
+        >
           <FaBeer className="titleIcon" />
           <p>Research</p>
         </li>
-        <li className="title">
+        <li
+          className="title"
+          id="Data Analysis"
+          onClick={() => addTitle("Data Analysis")}
+        >
           <FaBeer className="titleIcon" />
           <p>Data Analysis</p>
         </li>
-        <li className="title">
+        <li
+          className="title"
+          id="UI Design"
+          onClick={() => addTitle("UI Design")}
+        >
           <FaBeer className="titleIcon" />
           <p>UI Design</p>
         </li>
-        <li className="title">
+        <li
+          className="title"
+          id="UX Design"
+          onClick={() => addTitle("UX Design")}
+        >
           <FaBeer className="titleIcon" />
           <p>UX Design</p>
         </li>
-        <li className="title">
+        <li
+          className="title"
+          id="Technology"
+          onClick={() => addTitle("Technology")}
+        >
           <FaBeer className="titleIcon" />
           <p>Technology</p>
         </li>
-        <li className="title">
+        <li
+          className="title"
+          id="Creative"
+          onClick={() => addTitle("Creative")}
+        >
           <FaBeer className="titleIcon" />
           <p>Creative</p>
         </li>

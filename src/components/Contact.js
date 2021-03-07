7317fe164735a52e305e705554f5c260aa070871
faiza203 <60883181+faiza203@users.noreply.github.com>
2021-03-07 10:39:05 -0800

    Create structure of contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Contact = () => {
-  return <div id="Contact">Contact</div>;
+  return (
+    <div id="Contact">
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;

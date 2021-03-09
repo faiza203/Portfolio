a9848054aeaa9d216cd9d370bf78db778ee32f84
faiza203 <fiazfaiza981@gmail.com>
2021-03-09 11:59:29 -0500

    Complete About Page

--- src/components/About.js
+++ src/components/About.js
@@ -7,17 +7,14 @@ const About = () => {
     <div className="aboutPage" id="About">
       <div className="aboutMe d-inline-flex">
         <div className="developerImage ms-5 mt-2"></div>
-        <div className="text-muted mt-3 mb-3">
-          <h3 className="h3 mt-5">
-            I have great experience in
-            <br />
+        <div className="text-muted mt-5 mb-3">
+          <h4 className="h4 mt-5 text-muted experience">
+            I have great experience in {" "}
             <span className="parrot">
-              HTML , CSS , JS , Node JS , Express JS,
-              <br />
-              React , React Native , TypeScript ,
-              <br /> GatsBy , Linux , Docker and Kubernetes
+              HTML , CSS , JS , Node JS , Express JS, React , React Native ,
+              TypeScript , GatsBy , Linux , Docker and Kubernetes
             </span>
-          </h3>
+          </h4>
         </div>
       </div>
       <Staff />
--- src/components/Staff.js
+++ src/components/Staff.js
@@ -4,17 +4,17 @@ import "./css/staff.css";
 const Staff = () => {
   return (
     <div className="staff">
-      <h1 className="h1 text-muted mb-3">Our Staff</h1>
-      <div className="staffDetail">
+      <h1 className="h1 text-muted mb-3  mt-5">Our Staff</h1>
+      <div className="staffDetail mt-5">
         <div>
           <div className="amirImage backgroundImgDiv"></div>
-          <p className="text-muted mt-3">Amir Sadiq Ali</p>
-          <p className=" parrot">BlockChain Developer</p>
+          <h3 className="text-muted mt-3">Amir Sadiq Ali</h3>
+          <h5 className=" parrot">BlockChain Developer</h5>
         </div>
         <div>
           <div className="talhaImage backgroundImgDiv"></div>
-          <p className="text-muted mt-3">Talha Sadiq Ali</p>
-          <p className="parrot">Cloud Developer</p>
+          <h3 className="text-muted mt-3">Talha Sadiq Ali</h3>
+          <h5 className="parrot">Cloud Developer</h5>
         </div>
       </div>
     </div>

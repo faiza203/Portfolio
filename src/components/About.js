1f4c46e1bb3daefc513cf1fd434d2a25e5625154
faiza203 <fiazfaiza981@gmail.com>
2021-03-09 11:32:08 -0500

    Complete contact sites

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <div className="aboutPage" id="About">
       <div className="aboutMe d-inline-flex">
-        <div className="developerImage ml-5 mt-2"></div>
+        <div className="developerImage ms-5 mt-2"></div>
         <div className="text-muted mt-3 mb-3">
           <h3 className="h3 mt-5">
             I have great experience in
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,27 +3,37 @@ import "./css/contact.css";
 
 const Contact = () => {
   return (
-    <div id="Contact">
-      <div className="d-inline-flex">
-        <div id="contactSite">
-          <span className="material-icons-outlined contactIcon">facebook</span>
-          <h1>FaceBook ID</h1>
-          <p>Faiza Fiaz 315</p>
-        </div>
-        <div id="contactSite">
-          <span className="material-icons-outlined contactIcon">email</span>
-          <h1>Email</h1>
-          <p>fiazfaiza981@gmail.com</p>
-        </div>
-        <div id="contactSite">
+    <div id="Contact" className="mt-5">
+      <p className="fs-5 fw-lighter parrot">Contact</p>
+      <h2 className="text-uppercase mt-3 fw-normal">Contact Us</h2>
+      <div className="d-inline-flex mt-5">
+        <div id="contactSite" className="ms-2 me-4 rounded">
           <span className="material-icons-outlined contactIcon">home</span>
-          <h1>ADDRESS</h1>
-          <p>Faiza</p>
+          <h5>ADDRESS</h5>
+          <p id="contactData" className="mt-3 mb-3">
+            Nafess Tailors Manawala Faisalabad Pakistan
+          </p>
         </div>
-        <div id="contactSite">
+        <div id="contactSite" className="ms-2 me-4 rounded">
           <span className="material-icons-outlined contactIcon">phone</span>
-          <h1>Contact No</h1>
-          <p>Faiza</p>
+          <h5>Contact Number</h5>
+          <p id="contactData" className="mt-3 mb-3">
+            + 0320 54555203
+          </p>
+        </div>
+        <div id="contactSite" className="ms-2 me-4 rounded">
+          <span className="material-icons-outlined contactIcon">email</span>
+          <h5>Email ADDRESS</h5>
+          <p id="contactData" className="mt-3 mb-3">
+            fiazfaiza981@gmail.com
+          </p>
+        </div>
+        <div id="contactSite" className="ms-2 me-4 rounded">
+          <span className="material-icons-outlined contactIcon">facebook</span>
+          <h5>FaceBook ID</h5>
+          <p id="contactData" className="mt-3 mb-3">
+            Faiza.Fiaz.315
+          </p>
         </div>
       </div>
       <div>

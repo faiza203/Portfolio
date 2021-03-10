12fd230b10bfe2451361357a55d69796d6789a59
faiza203 <fiazfaiza981@gmail.com>
2021-03-10 15:18:00 -0500

    Update Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,31 +6,31 @@ const Contact = () => {
     <div id="Contact" className="mt-5">
       <p className="fs-5 fw-lighter parrot">Contact</p>
       <h2 className="text-uppercase mt-3 fw-normal">Contact Us</h2>
-      <div className="d-inline-flex mt-5">
-        <div id="contactSite" className="ms-2 me-4 rounded">
+      <div className="d-inline-flex mt-5 mb-5">
+        <div id="contactSite" className="ms-4 me-4 rounded">
           <span className="material-icons-outlined contactIcon">home</span>
-          <h5>ADDRESS</h5>
+          <h5 className="fw-bold">ADDRESS</h5>
           <p id="contactData" className="mt-3 mb-3">
             Nafess Tailors Manawala Faisalabad Pakistan
           </p>
         </div>
-        <div id="contactSite" className="ms-2 me-4 rounded">
+        <div id="contactSite" className="ms-4 me-4 rounded">
           <span className="material-icons-outlined contactIcon">phone</span>
-          <h5>Contact Number</h5>
+          <h5 className="fw-bold">Contact Number</h5>
           <p id="contactData" className="mt-3 mb-3">
             + 0320 54555203
           </p>
         </div>
-        <div id="contactSite" className="ms-2 me-4 rounded">
+        <div id="contactSite" className="ms-4 me-4 rounded">
           <span className="material-icons-outlined contactIcon">email</span>
-          <h5>Email ADDRESS</h5>
+          <h5 className="fw-bold">Email ADDRESS</h5>
           <p id="contactData" className="mt-3 mb-3">
             fiazfaiza981@gmail.com
           </p>
         </div>
-        <div id="contactSite" className="ms-2 me-4 rounded">
+        <div id="contactSite" className="ms-4 me-4 rounded">
           <span className="material-icons-outlined contactIcon">facebook</span>
-          <h5>FaceBook ID</h5>
+          <h5 className="fw-bold">FaceBook ID</h5>
           <p id="contactData" className="mt-3 mb-3">
             Faiza.Fiaz.315
           </p>

9d59d273a53e8e6d9355eb5c397307cb1615bbd8
faiza203 <fiazfaiza981@gmail.com>
2021-03-11 18:27:44 -0500

    Update Projects dots

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,10 +3,25 @@ import "./css/projects.css";
 
 let imgClasses = ["chat-app", "todo-app", "weather-app", "weather-app-2"];
 function setImages(imgApp) {
-  imgClasses.forEach(imgClass => {
-    imgClass !== imgApp ? document.getElementById(imgClass).classList.add("projectBackgroundImageActive") : document.getElementById(imgApp).classList.remove("projectBackgroundImageActive")
-    imgClass !== imgApp ? document.getElementById(imgClass).classList.remove("projectBackgroundImage") : document.getElementById(imgApp).classList.add("projectBackgroundImage")
-  })
+  imgClasses.forEach((imgClass) => {
+    imgClass !== imgApp
+      ? document
+          .getElementById(imgClass)
+          .classList.add("projectBackgroundImageActive")
+      : document
+          .getElementById(imgApp)
+          .classList.remove("projectBackgroundImageActive");
+    imgClass !== imgApp
+      ? document
+          .getElementById(imgClass)
+          .classList.remove("projectBackgroundImage")
+      : document.getElementById(imgApp).classList.add("projectBackgroundImage");
+    imgClass !== imgApp
+      ? document
+          .getElementById(imgClass+"-btn")
+          .classList.remove("active")
+      : document.getElementById(imgApp+"-btn").classList.add("active");
+  });
 }
 
 const Projects = () => {
@@ -16,16 +31,48 @@ const Projects = () => {
       <div
         className="d-flex projectImages mt-5 justify-content-center mb-5"
         id="projectImages"
-      >  <div className="projectBackgroundImageActive chat-app" id="chat-app"><h1>Chat App</h1></div>
-         <div className="projectBackgroundImageActive weather-app" id="weather-app"><h1>Weather App</h1></div>
-         <div className="projectBackgroundImageActive todo-app " id="todo-app"><h1>Todo App</h1></div>
-        <div className=" projectBackgroundImage weather-app-2" id="weather-app-2"><h1>Weather App 2</h1></div>
+      >
+        {" "}
+        <div className="projectBackgroundImageActive chat-app" id="chat-app">
+          <h1>Chat App</h1>
+        </div>
+        <div
+          className="projectBackgroundImageActive weather-app"
+          id="weather-app"
+        >
+          <h1>Weather App</h1>
+        </div>
+        <div className="projectBackgroundImageActive todo-app " id="todo-app">
+          <h1>Todo App</h1>
+        </div>
+        <div
+          className=" projectBackgroundImage weather-app-2"
+          id="weather-app-2"
+        >
+          <h1>Weather App 2</h1>
+        </div>
       </div>
       <div className="dots d-inline-flex justify-content-center mt-5">
-         <button className="btn me-2" onClick={() => setImages("todo-app")}></button>
-        <button className="btn me-2" onClick={() => setImages("chat-app")}></button>
-        <button className="btn me-2" onClick={() => setImages("weather-app-2")}></button>
-        <button className="btn me-2" onClick={() => setImages("weather-app")}></button>
+        <button
+          className="btn me-2 "
+          id="todo-app-btn"
+          onClick={() => setImages("todo-app")}
+        ></button>
+        <button
+          id="chat-app-btn"
+          className="btn me-2"
+          onClick={() => setImages("chat-app")}
+        ></button>
+        <button
+          id="weather-app-2-btn"
+          className="btn me-2"
+          onClick={() => setImages("weather-app-2")}
+        ></button>
+        <button
+          id="weather-app-btn"
+          className="btn me-2"
+          onClick={() => setImages("weather-app")}
+        ></button>
       </div>
     </div>
   );

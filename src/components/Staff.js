ccec34ae95de8c05588c7fb78591a480a9d91935
faiza203 <fiazfaiza981@gmail.com>
2021-03-13 12:36:42 -0500

    Add staff Data

--- src/components/Staff.js
+++ src/components/Staff.js
@@ -1,19 +1,6 @@
 import React from "react";
 import "./css/staff.css";
 
-const GoBack = ({ id }) => {
-  return (
-    <button
-      className="btn btn-outline-primary"
-      onClick={() => {
-        return (id = "amir" ? <div></div> : <div></div>);
-      }}
-    >
-      Done
-    </button>
-  );
-};
-
 const Staff = () => {
   return (
     <div className="staff  pb-5">
@@ -24,20 +11,33 @@ const Staff = () => {
           <h2 className="text-muted mt-5">Amir Sadiq Ali</h2>
           <h4 className=" parrot">BlockChain Developer</h4>
           <div id="amirData">
-            <span className="material-icons-outlined">phone</span>
-            <span className="material-icons-outlined me-4 ms-4">facebook</span>
+            <span>
+              <a href="https://github.com/ammy-bajwa">
+                <svg
+                  class="octicon octicon-mark-github v-align-middle"
+                  height="24"
+                  viewBox="0 0 16 16"
+                  version="1.1"
+                  width="24"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+                  ></path>
+                </svg>
+              </a>
+            </span>
+            <span className="material-icons-outlined me-4 ms-4">
+              <a href="https://www.facebook.com/amirsadiq.ali.10/photos_albums">
+                facebook
+              </a>
+            </span>
             <span
               className="material-icons-outlined amir-email"
-              onClick={() => {
-               return document.getElementById("amirData").innerHTML = (
-                  <div>
-                    <h1>amirsadiqalisaifi@gmail.com</h1>
-                    <GoBack id="amir"/>
-                  </div>
-                );
-              }}
+              onClick={() => {}}
             >
-              email
+              <a href="mailto:amirsadiqalisaifi@gmail.com"> email</a>
             </span>
           </div>
         </div>
@@ -45,10 +45,30 @@ const Staff = () => {
           <div className="talhaImage backgroundImgDiv"></div>
           <h2 className="text-muted mt-5">Talha Sadiq Ali</h2>
           <h4 className="parrot ">Cloud Developer</h4>
-          <div>
-            <span className="material-icons-outlined">phone</span>
-            <span className="material-icons-outlined me-4 ms-4">facebook</span>
-            <span className="material-icons-outlined">email</span>
+          <div id="talhaData">
+            <span>
+              <a href="https://github.com/MuhammadTalha123">
+                <svg
+                  class="octicon octicon-mark-github v-align-middle"
+                  height="24"
+                  viewBox="0 0 16 16"
+                  version="1.1"
+                  width="24"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+                  ></path>
+                </svg>
+              </a>
+            </span>
+            <span className="material-icons-outlined me-4 ms-4">
+              <a href="https://www.facebook.com/talha.bajwa.522">facebook</a>
+            </span>
+            <span className="material-icons-outlined">
+              <a href="mailto:mt8825130@gmail.com">email</a>
+            </span>
           </div>
         </div>
       </div>

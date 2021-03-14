08493e0e6f0c7b1cfccb82c1ce46935e55b4b794
faiza203 <fiazfaiza981@gmail.com>
2021-03-14 15:57:36 -0400

    Complete Style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const currentDate = new Date();
 
 export default function Footer() {
   return (
-    <h1 className="footer" style={{ background: "black" , color : "white" , width: "100vw" , padding : "1em 0" }}>
+    <h1 className="footer" style={{ background: "black" , color : "white" ,padding : "1em 0" }}>
       All rights are reserved <span className="curcle" style={{color : "aqua" , padding: "0px 10px 4px 10px" , border : "1px solid aqua" , borderRadius : "50%" }}>c</span> {currentDate.getFullYear()}
     </h1>
   );
